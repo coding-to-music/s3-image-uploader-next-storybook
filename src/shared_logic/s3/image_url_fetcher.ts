7980b2e5c5fe68ce57ec28966f4e653ec376fc40
coding-to-music <connors.tom@gmail.com>
2022-11-22 21:39:54 +0000

    Build

--- src/shared_logic/s3/image_url_fetcher.ts
+++ src/shared_logic/s3/image_url_fetcher.ts
@@ -1,27 +1,35 @@
 import { ListObjectsCommand, S3Client } from '@aws-sdk/client-s3';
 import { createImageUrl } from './image_url_creator';
 
-console.log("image_url_fetcher: createImage_Url: ");
+console.log('image_url_fetcher: createImage_Url: ');
+
+console.log('image_url_fetcher: process.env.AWS_S3_BUCKET_NAME ', process.env.AWS_S3_BUCKET_NAME);
 
 async function fetchObjectKeys(client: S3Client): Promise<string[]> {
   const command = new ListObjectsCommand({
     Bucket: process.env.AWS_S3_BUCKET_NAME,
   });
 
+  console.log('image_url_fetcher: command ', command);
+
   try {
     const response = await client.send(command);
     const keys = response.Contents.map((content) => content.Key);
+    console.log('image_url_fetcher: keys ', keys);
     return keys;
   } catch (err) {
     console.error(err);
   }
+
+
 }
 
 export async function fetchImageUrlList(client: S3Client, secondsToExpire: number): Promise<string[]> {
   const objectKeys = await fetchObjectKeys(client);
-  const imageUrlList = await Promise.all(
-    objectKeys.map(async (key) => await createImageUrl({ imagePath: key, secondsToExpire })),
-  );
+  console.log('image_url_fetcher: objectKeys ', objectKeys);
+  // const imageUrlList = await Promise.all(
+  //   objectKeys.map(async (key) => await createImageUrl({ imagePath: key, secondsToExpire })),
+  // );
 
   return imageUrlList;
 }
